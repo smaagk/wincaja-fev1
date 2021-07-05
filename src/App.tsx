95498eb3efdd0b5a49ef0b7cff8867e50d919285
smaagk <smaa.gk@gmail.com>
2021-07-05 16:35:45 -0500

    acceso solo a clientes

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,16 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import HomeAdmin from './components/admin/home-admin';
 import Catalog from './components/catalog/catalog';
 import AuthAdmin from './guards/auth-guard';
+import AuthClient from 'guards/client-guard';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Route path="/" component={Catalog} exact />
+        <AuthClient path="/" component={Catalog} exact />
         <AuthAdmin path="/admin" component={HomeAdmin} />
         <Route path="/login" component={AccessComponent} exact />
-        <Route path="/tienda" component={Catalog} />
+        <AuthClient path="/tienda" component={Catalog} />
         <Route path="/debug" component={TypeaHead} />
       </div>
     </BrowserRouter>
